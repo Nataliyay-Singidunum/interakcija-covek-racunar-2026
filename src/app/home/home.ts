a5ffce4df16272723a1c6980197047200eb52f45
Natalija Jolovic <natalijajolovic@gmail.com>
2026-08-06 03:43:35 +0200

    Connected to the movie api, created the service and model for it and displayed movies on the front page

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MovieService } from '../../services/movie.service';
+import { MovieModel } from '../../models/movie.model';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+  protected movies = signal<MovieModel[]>([]);
+
+  constructor() {
+    MovieService.getMovies().then(rsp => this.movies.set(rsp.data));
+  }
+}
